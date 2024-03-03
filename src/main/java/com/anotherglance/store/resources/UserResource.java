8e932f02623e641f96e1c33dcdbbb4b69a168278
João Vitor Moreira dos Santos Neris <joaomoreiraneris0@gmail.com>
2024-03-03 14:05:50 -0300

    User entity and resource

---
+++ src/main/java/com/anotherglance/store/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.anotherglance.store.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anotherglance.store.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "João", "email@gmail.com", "996464564616", "senha");
+		return ResponseEntity.ok().body(u);
+	}
+}
